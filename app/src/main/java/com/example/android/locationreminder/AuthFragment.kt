cebf28c1a2afb3366c55df5f5152402b6ca92f4f
CalixtoElProgramador <correo.basura0811@gmail.com>
2021-10-26 11:23:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/locationreminder/AuthFragment.kt
@@ -0,0 +1,16 @@
+package com.example.android.locationreminder
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+
+class AuthFragment : Fragment(R.layout.fragment_auth) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
